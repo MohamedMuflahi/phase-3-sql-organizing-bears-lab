fcc0ee495ec5ec009dee0b229cf3d854f2333bd6
mohamed muflahi <muflahimohamed88@gmail.com>
2022-03-17 21:08:49 -0400

    commit tree acb55ed1729d476069df39fcfb5aefe475596ec4

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Mr. Chocolate',18,'M','red','good',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Rowdy',18,'M','red','good',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Tabitha',18,'M','red','good',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Sergeant Brown',18,'M','red','good',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Melissa',18,'M','red','good',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Grinch',18,'M','red','good',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ('Wendy',18,'M','red','good',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL,18,'M','red','good',0);
+
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
